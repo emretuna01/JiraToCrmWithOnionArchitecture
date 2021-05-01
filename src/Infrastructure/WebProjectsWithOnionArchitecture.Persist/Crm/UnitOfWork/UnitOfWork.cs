b8aa6cc58fe387dc11119183b4e51f130d0c4289
Emre Tuna <emretuna@outlook.com>
2021-05-01 22:49:33 +0300

    BeginTransactionAsync added

---
+++ src/Infrastructure/WebProjectsWithOnionArchitecture.Persist/Crm/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProjectsWithOnionArchitecture.Application.Crm.Interfaces.DbRepository;
+using WebProjectsWithOnionArchitecture.Application.Crm.Interfaces.UnitOfWork;
+using WebProjectsWithOnionArchitecture.Persist.DbContext;
+
+namespace WebProjectsWithOnionArchitecture.Persist.Crm.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        readonly ApplicationDbContext _applicationDbContext;
+
+        public UnitOfWork(ApplicationDbContext applicationDbContext, ICrmUserRepository crmUserRepository)
+        {
+            _applicationDbContext = applicationDbContext;
+            CrmUserRepository = crmUserRepository;
+        }
+
+        public ICrmUserRepository CrmUserRepository { get; }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await _applicationDbContext.Database.BeginTransactionAsync();
+        }
+    }
+}
