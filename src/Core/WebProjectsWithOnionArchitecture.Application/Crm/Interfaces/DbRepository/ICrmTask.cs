7680151ef8135f7f33b6dca8d0c80dadab06a226
Emre Tuna <emretuna@outlook.com>
2021-05-03 00:46:36 +0300

    CrmTaskRepository added

---
+++ src/Core/WebProjectsWithOnionArchitecture.Application/Crm/Interfaces/DbRepository/ICrmTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProjectsWithOnionArchitecture.Domain.Crm.Entities;
+
+namespace WebProjectsWithOnionArchitecture.Application.Crm.Interfaces.DbRepository
+{
+    public interface ICrmTask :IDbRepository<CrmTask>
+    {
+    }
+}
---
+++ src/Infrastructure/WebProjectsWithOnionArchitecture.Persist/Crm/DbRepositories/CrmTaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProjectsWithOnionArchitecture.Application.Crm.Interfaces.DbRepository;
+using WebProjectsWithOnionArchitecture.Domain.Crm.Entities;
+using WebProjectsWithOnionArchitecture.Persist.Crm.Repositories;
+using WebProjectsWithOnionArchitecture.Persist.DbContext;
+
+namespace WebProjectsWithOnionArchitecture.Persist.Crm.DbRepositories
+{
+    public class CrmTaskRepository : DbRepository<CrmTask>, ICrmTask
+    {
+        public CrmTaskRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
