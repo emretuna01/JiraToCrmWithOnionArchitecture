6228f64a60b90cb5111633e7acea0e7777e6c246
Emre Tuna <emretuna@outlook.com>
2021-05-04 02:32:52 +0300

    CrmSalesOrder added

--- src/Core/WebProjectsWithOnionArchitecture.Domain/Crm/Entities/CrmTask.cs
+++ src/Core/WebProjectsWithOnionArchitecture.Domain/Crm/Entities/CrmTask.cs
@@ -8,14 +8,12 @@ using WebProjectsWithOnionArchitecture.Domain.Crm.Common;
 namespace WebProjectsWithOnionArchitecture.Domain.Crm.Entities
 {
     public class CrmTask: BaseCrmEntity
-    {
-    
+    {   
         public string Subject { get; set; }
         public string Description { get; set; }
         public string RegardingObjectidSalesOrderOdataBind { get; set; }
         public DateTimeOffset Scheduledend { get; set; }
         public string categoryOfTask { get; set; }
-
 
     }
 }
