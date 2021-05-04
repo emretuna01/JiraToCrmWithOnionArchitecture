ae1b878c2766d653ddab3875c3f8b7f9fd889f65
Emre Tuna <emretuna@outlook.com>
2021-05-05 01:15:47 +0300

    Sqlite entity framework added

--- src/Infrastructure/WebProjectsWithOnionArchitecture.Persist/ServiceRegistration.cs
+++ src/Infrastructure/WebProjectsWithOnionArchitecture.Persist/ServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +16,7 @@ namespace WebProjectsWithOnionArchitecture.Persist
     {
         public static void AddPersistanceServices(this IServiceCollection serviceCollection)
         {
-            //serviceCollection.AddDbContext<ApplicationDbContext>(dbContextOptions => dbContextOptions.UseSqlServer)
+            serviceCollection.AddDbContext<ApplicationDbContext>(dbContextOptions => dbContextOptions.UseSqlite());
             serviceCollection.AddTransient<ICrmUserRepository, CrmUserRepository>();
         }
 
