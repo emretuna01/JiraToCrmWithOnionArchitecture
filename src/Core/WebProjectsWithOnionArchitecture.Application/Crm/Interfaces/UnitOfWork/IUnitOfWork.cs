4997db9bc801159c3b18aa0f1539e867f2748dbb
Emre Tuna <emretuna@outlook.com>
2021-05-05 01:02:19 +0300

    Concrede class of UnitOfWork added

--- src/Core/WebProjectsWithOnionArchitecture.Application/Crm/Interfaces/UnitOfWork/IUnitOfWork.cs
+++ src/Core/WebProjectsWithOnionArchitecture.Application/Crm/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -10,7 +10,7 @@ namespace WebProjectsWithOnionArchitecture.Application.Crm.Interfaces.UnitOfWork
 {
     public interface IUnitOfWork
     {
-        Task<IDbContextTransaction> BeginTransactionAsync();        
+        public Task<IDbContextTransaction> BeginTransactionAsync();        
         public ICrmUserRepository CrmUserRepository { get;}
         public ICrmAccountRepository CrmAccountRepository { get; }
         public ICrmTaskRepository CrmTaskRepository { get; }
--- src/Infrastructure/WebProjectsWithOnionArchitecture.Persist/Crm/UnitOfWork/UnitOfWork.cs
+++ src/Infrastructure/WebProjectsWithOnionArchitecture.Persist/Crm/UnitOfWork/UnitOfWork.cs
@@ -14,13 +14,24 @@ namespace WebProjectsWithOnionArchitecture.Persist.Crm.UnitOfWork
     {
         readonly ApplicationDbContext _applicationDbContext;
 
-        public UnitOfWork(ApplicationDbContext applicationDbContext, ICrmUserRepository crmUserRepository)
+        public UnitOfWork(ApplicationDbContext applicationDbContext, 
+                          ICrmUserRepository crmUserRepository, 
+                          ICrmAccountRepository crmAccountRepository, 
+                          ICrmTaskRepository crmTaskRepository,
+                          ICrmSalesOrderRepository crmSalesOrderRepository)
         {
             _applicationDbContext = applicationDbContext;
             CrmUserRepository = crmUserRepository;
+            CrmAccountRepository = crmAccountRepository;
+            CrmTaskRepository = crmTaskRepository;
+            CrmSalesOrderRepository = crmSalesOrderRepository;
+
         }
 
         public ICrmUserRepository CrmUserRepository { get; }
+        public ICrmAccountRepository CrmAccountRepository { get; }
+        public ICrmTaskRepository CrmTaskRepository { get; }
+        public ICrmSalesOrderRepository CrmSalesOrderRepository { get; }
 
         public async Task<IDbContextTransaction> BeginTransactionAsync()
         {
