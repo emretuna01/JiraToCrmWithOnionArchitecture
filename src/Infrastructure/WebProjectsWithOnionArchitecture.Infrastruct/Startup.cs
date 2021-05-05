8e56a566226caffd770cea8887c80a47b7a5161e
Emre Tuna <emretuna@outlook.com>
2021-05-06 00:44:02 +0300

    Infrastructure Layer changed to web proj

---
+++ src/Infrastructure/WebProjectsWithOnionArchitecture.Infrastruct/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProjectsWithOnionArchitecture.Infrastruct
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get;}
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder,IWebHostEnvironment webHostEnvironment)
+        {
+
+        }
+
+    }
+}
--- src/Presentation/WebProjectsWithOnionArchitecture.Present/Program.cs
+++ src/Presentation/WebProjectsWithOnionArchitecture.Present/Program.cs
@@ -16,11 +16,15 @@ namespace WebProjectsWithOnionArchitecture.Present
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+
+            return Host.CreateDefaultBuilder(args).
+                        ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.UseStartup<Startup>();
+                        });
+        }
+
     }
 }
