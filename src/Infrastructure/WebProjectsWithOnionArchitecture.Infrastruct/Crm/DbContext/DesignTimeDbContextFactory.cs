1c5c97b9264cb6f9cfee794e25d1313028ed56c4
Emre Tuna <emretuna@outlook.com>
2021-05-07 02:01:58 +0300

    DesignTimeDbContextFactory class added

---
+++ src/Infrastructure/WebProjectsWithOnionArchitecture.Infrastruct/Crm/DbContext/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProjectsWithOnionArchitecture.Persist.DbContext;
+
+namespace WebProjectsWithOnionArchitecture.Infrastruct.Crm.DbContext
+{
+    public class DesignTimeDbContextFactory<TContext> : IDesignTimeDbContextFactory<TContext> where TContext : ApplicationDbContext
+    {
+        public TContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<TContext> dbContextOptionsBuilder = new DbContextOptionsBuilder<TContext>();
+            IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../WebProjectsWithOnionArchitecture.Persist")).AddJsonFile("appsettings.json").Build();
+            dbContextOptionsBuilder.UseSqlite(configuration.GetConnectionString("SqlLite"));
+        }
+    }
+}
